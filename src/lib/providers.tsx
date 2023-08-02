a1cd44902262cad0ea811edff311fefe842db2ad
Mehradi <mehradi.info@gmail.com>
2023-08-02 12:31:32 +0200

    Adding ApolloClient

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,11 +1,21 @@
-'use client'
+"use client";
 
 /* Core */
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 
 /* Instruments */
-import { reduxStore } from '@/lib/redux'
+import { reduxStore } from "@/lib/redux";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:3002/graphql",
+  cache: new InMemoryCache(),
+});
 
 export const Providers = (props: React.PropsWithChildren) => {
-  return <Provider store={reduxStore}>{props.children}</Provider>
-}
+  return (
+    <Provider store={reduxStore}>
+      <ApolloProvider client={client}>{props.children}</ApolloProvider>
+    </Provider>
+  );
+};
