d68eec3b546f99afb9019d12646fd4356b037ab2
Mehradi <mehradi.info@gmail.com>
2023-08-02 12:28:24 +0200

    Moving graphql dir into lib dir

--- codegen.ts
+++ codegen.ts
@@ -2,10 +2,10 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "http://localhost:3002/graphql",
-  documents: ["src/graphql/client/**/*.{ts,tsx}"],
+  documents: ["src/lib/graphql/client/**/*.{ts,tsx}"],
   ignoreNoDocuments: true,
   generates: {
-    "./src/graphql/gql/": {
+    "./src/lib/graphql/gql/": {
       preset: "client",
       plugins: [],
     },
